a951adcff3a3b7c0945778c5823882850df5c5ad
Ludovico Ivaldi <144495896+ludovicoivaldi-academic@users.noreply.github.com>
2025-03-08 18:54:46 +0100

    Update quartz.config.ts

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Germogli di Matematica 🌱",
+    pageTitle: "Germogli di Matematica🌱",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
